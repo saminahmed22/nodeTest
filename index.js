d6a2d34606e2478039374481d3628c09334d71cf
Samin Ahmed <saminahmed1001@gmail.com>
2026-04-29 19:53:04 +0600

    .

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log("Request made");
+
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    case "/contact-me":
+      path += "contact-me.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  // send a html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("Listening");
+});
